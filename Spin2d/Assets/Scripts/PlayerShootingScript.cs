7c54dad62af547212f5b618206cc8b7bae36e6c7
DauletMaker <eco.bober@mail.ru>
2021-01-31 16:26:32 +0600

    CinemachineAndShootingDone

---
+++ Spin2d/Assets/Scripts/PlayerShootingScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootingScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public GameObject BulletPoint;
+    public GameObject bullet;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Instantiate(bullet, BulletPoint.transform.position, BulletPoint.transform.rotation);
+            Debug.Log("tRUBI");
+
+            rb.AddForce(transform.up * 1000f);
+        }
+    }
+}
