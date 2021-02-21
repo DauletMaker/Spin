9f2c2925c356705c304a838757300be5464bb252
DauletMaker <eco.bober@mail.ru>
2021-02-21 22:28:15 +0600

    AmmoBarAdded

--- Spin2d/Assets/Scripts/PlayerShootingScript.cs
+++ Spin2d/Assets/Scripts/PlayerShootingScript.cs
@@ -8,20 +8,58 @@ public class PlayerShootingScript : MonoBehaviour
     public GameObject BulletPoint;
     public GameObject bullet;
 
+    public Animator BulletBar;
+
+    public int ammo;
+    public bool CanShoot;
+
     void Start()
     {
-        
+        ammo = 5;
+        CanShoot = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
+        if (Input.GetKeyDown(KeyCode.G) && ammo > 0 && CanShoot == true)
         {
             Instantiate(bullet, BulletPoint.transform.position, BulletPoint.transform.rotation);
             Debug.Log("tRUBI");
 
             rb.AddForce(transform.up * 1000f);
+            ammo--;
+            BulletBar.SetTrigger("Ammo--");
+        }
+
+        if(ammo == 0)
+        {
+            StartCoroutine(AmmoRegenerate());
+            CanShoot = false;
+        }
+    }
+
+    public IEnumerator AmmoRegenerate()
+    {
+        if(ammo < 5)
+        {
+            yield return new WaitForSeconds(0.4f);
+            ammo = 1;
+            BulletBar.Play("Health1");
+            yield return new WaitForSeconds(0.4f);
+            ammo = 2;
+            BulletBar.Play("Health2");
+            yield return new WaitForSeconds(0.4f);
+            ammo = 3;
+            BulletBar.Play("Health3");
+            yield return new WaitForSeconds(0.4f);
+            ammo = 4;
+            BulletBar.Play("Health4");
+            yield return new WaitForSeconds(0.4f);
+            ammo = 5;
+            BulletBar.Play("Health5");
+            CanShoot = true;
+
         }
     }
 }
